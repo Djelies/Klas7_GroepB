b8e7854dd7561bd8570a6574adfc3d602af0c9e9
Djelies <djelies.msk@hotmail.nl>
2021-12-20 14:37:07 +0100

    Git commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MijnProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
